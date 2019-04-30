1593f134267030060bc69fd68813492618f7e440
JasonLouw <mjasonlouw@gmail.com>
2019-04-30 13:03:37 +0200

    Added mesh functionality and agents
    3d modeled basic room

---
+++ MapUnity/Assets/AgentController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+         agent.SetDestination(GameObject.FindWithTag("door1").transform.position);
+        // agent.destination = new Vector3(1,1,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
